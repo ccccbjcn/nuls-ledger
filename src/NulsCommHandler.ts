96c2113c35e5cf2427f5b7ad01e68005596edc3e
Andrea B <vekexasia@gmail.com>
2019-04-25 16:40:09 +0200

    :bug: probable fix u2f

--- src/NulsCommHandler.ts
+++ src/NulsCommHandler.ts
@@ -1,6 +1,12 @@
 import { CommHandler } from 'dpos-ledger-api';
+import { ITransport } from 'dpos-ledger-api/src/ledger';
 
 export class NulsCommHandler extends CommHandler {
+  constructor(transport: ITransport, chunkSize: number = 240) {
+    super(transport, chunkSize);
+    transport.setScrambleKey('NULS');
+  }
+
   protected prepareStartCommBufferContent(inputBuffer: Buffer): Buffer {
     // super method only writes 2 bytes.
     const buff = Buffer.alloc(4);
